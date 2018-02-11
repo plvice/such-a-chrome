410cbfb0c0ce2c152d8a0c95f91af08af5d4ef7d
Piotr <prawdziwypiotrek@gmail.com>
2018-02-11 17:30:54 +0100

    Implement file uploader

--- pdf-by-downloads/content.js
+++ pdf-by-downloads/content.js
@@ -1,10 +1,11 @@
 const guid = 'c884efbe-1c7d-408c-ad42-85efa56cb78f';
+const extensionId = 'oiljhhljfdjngigfghkiloeedgjkmddl';
 const container = document.createElement('div');
 const persistence = 120;
+let files = [];
 
 container.classList.add(`${guid}-container`);
 document.body.appendChild(container);
-
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request) {
@@ -12,13 +13,15 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     }
 });
 
-function update(files) {
+function update(request) {
     let html = '';
+    files = request;
+
     files.forEach(file => {
         if (file.id) {
             html += 
-            `<div class="${guid}-file" data-id="${guid}-${file.id}">
-                <div class="${guid}-filename">${file.id}</div>
+            `<div class="${guid}-file" data-id="${file.id}" draggable="true">
+                <div class="${guid}-filename">${file.id} <button type="button" data-button="${file.id}">Wyślij</button></div>
                 <div class="${guid}-filepersistence">
                     <span class="${guid}-sidebar" style="width: ${file.time/persistence*100}%;">
                         ${file.time}
@@ -28,4 +31,25 @@ function update(files) {
         }
     })
     container.innerHTML = html;
+    bindEvents();
+}
+
+function bindEvents() {
+    let items = document.querySelectorAll(`[data-button]`);
+
+    items.forEach(item => {
+        item.onclick = function() {
+            let id = item.getAttribute('data-button');
+            let file = files.filter((item) => {
+                return item.id.toString() === id;
+            })
+
+            console.log(`clicked file id ${id}`);
+            
+            chrome.runtime.sendMessage(extensionId, { path: file[0].path }, 
+                function responseCallback(response) {
+                    console.log(response);
+                })
+        }
+    })
 }
--- pdf-by-downloads/pdfHandler.js
+++ pdf-by-downloads/pdfHandler.js
@@ -9,21 +9,54 @@ chrome.downloads.onCreated.addListener(response => {
       id: response.id,
       time: persistenceTime
     })
-
-    sendFilesToView();
   }
 })
 
-//refresh persistence time
+chrome.downloads.onChanged.addListener(response => {
+  const fileIndex = files.findIndex((item) => {
+    return item.id === response.id;
+  })
+
+  if (fileIndex !== -1) {
+    if (response.filename && response.filename.current) {
+      files[fileIndex].path = response.filename.current;
+    }
+
+    if (response.state && response.state.current === 'complete') {
+      files[fileIndex].completed = true;
+    }
+  }
+})
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.path) {
+    xhrGET(message.path, function (success) {
+      console.log('success');
+      console.log(btoa(unescape(encodeURIComponent(success))).substr(0, 100));
+    },
+      function (err) {
+        console.log(err);
+      })
+  }
+})
+
+//state refresh
 setInterval(() => {
+
+  //can send only completed downloads.
+  files = files.filter(item => {
+    return item.completed;
+  })
+
+  //refresh persistence time and remove files that are out of the time limit
   if (files.length > 0) {
     files.forEach((file, index) => {
       file.time = file.time - 1;
 
       if (file.time <= 0) {
-        chrome.downloads.removeFile(file.id, function (response) {
+        chrome.downloads.removeFile(file.id, response => {
           console.log(`File with ID ${response.id} removed.`)
-        });
+        })
         files.splice(index, 1);
       }
     })
@@ -46,6 +79,31 @@ function isPDF(mime) {
   return mime.indexOf('pdf') !== -1;
 }
 
-// chrome.downloads.onChanged.addListener(response => {
-//   console.log(response);
-// });
+function xhrGET(url, onSuccess, onFailure) {
+  onSuccess = onSuccess || function _onSuccess(data) { };
+  onFailure = onFailure || function _onFailure() { };
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+
+  // if (xhr.overrideMimeType) {
+  //   xhr.overrideMimeType('text/plain; charset=utf-8');
+  // }
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200 || xhr.status === 0) {
+        onSuccess(xhr.responseText);
+      } else {
+        onFailure();
+      }
+    }
+  };
+  xhr.onerror = onFailure;
+  xhr.ontimeout = onFailure;
+  try {
+    xhr.send(null);
+  } catch (e) {
+    onFailure();
+  }
+}
